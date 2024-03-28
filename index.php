31a7fd20b41a30e9ef1d32198a0b8c3e95361bdd
Quinten <quinblom321@gmail.com>
2024-03-28 11:21:31 +0100

    Home page backend af
    Geen JS op deze pagina nodig

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <?php
     require_once "services/initialization.php";
-    $_SESSION['current_page'] = "index";
-
-    var_dump($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>EasyFind</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+    <section class="header-image">
+        <img src="img/result.brf.png" alt="Braille Image">
+    </section>
+    <h1 class="page-title">Home</h1>
+    <section class="home-navigation">
+        <div class="navigation-box">
+            <a href="pages/shoppinglist.php"><img src="img/grocery.png" alt="Boodschappen lijstje"></a>
+        </div>
+        <div class="navigation-box">
+            <a href="pages/products.php"><img src="img/fruit.png" alt="fruit"></a>
+        </div>
+        <div class="navigation-box">
+            <a href="pages/help.php"><img src="img/1200px-Icon-round-Question_mark.svg.png" alt="informatie"></a>
+        </div>
+        <div class="navigation-box">
+            <a href="pages/"><img src="img/store.jpg" alt="Winkels"></a>
+        </div>
+        <div class="navigation-box">
+            <a href="pages/store.php"><img src="img/location.png" alt="locatie"></a>
+        </div>
+        <div class="navigation-box">
+            <a href="pages/store-navigation.php"><img src="img/route.jpg" alt="Route"></a>
+        </div>
+    </section>
+    <footer class="footer">
+    <p>Privacy</p>
+    </footer>
 </body>
 </html>
