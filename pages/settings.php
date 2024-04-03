1c918c95d07306e86b4c17918ffd526afdb716e9
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-04-03 11:09:38 +0200

    settings en store

--- pages/settings.php
+++ pages/settings.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>EasyFind</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <script type="text/javascript" src="../js/stores.js" defer></script>
+    </head>
+<body>
+    <section class="header-image">
+        <img src="../img/result.brf.png" alt="Braille Image">
+    </section>
+    <img src="../img/settings.png">
+
+
+    <footer class="footer">
+        <p>Privacy</p>
+    </footer>
+</body>
+</html>
